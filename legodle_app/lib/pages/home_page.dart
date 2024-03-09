98504abd95a3c4805cca95c61070df301bd1335d
Riley Wong <rnwong@scu.edu>
2024-03-08 21:38:44 -0800

    Add comma

--- legodle_app/lib/pages/home_page.dart
+++ legodle_app/lib/pages/home_page.dart
@@ -31,136 +31,138 @@ class _HomePageState extends State<HomePage> {
     final Styles styles = Styles(context: context);
 
     return Scaffold(
-        body: SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.only(top: 40.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              context.read<GameProvider>().unlimitedMode
-                  ? 'Brickdle Unlimited'
-                  : 'Daily Brickdle #${context.read<GameProvider>().todaysNum}',
-              style: styles.titleTextStyle,
-              textAlign: TextAlign.center,
-              softWrap: true,
-            ),
-            Image.network(
-              currentLegoSet.imageUrl,
-              fit: BoxFit.fitWidth,
-              height: 300,
-              loadingBuilder: (context, child, loadingProgress) {
-                if (loadingProgress == null) {
-                  return child;
-                } else {
-                  return Center(
-                    child: SizedBox(
-                      width: 50,
-                      height: 300,
-                      child: Padding(
-                        padding: const EdgeInsets.only(top: 125, bottom: 125),
-                        child: CircularProgressIndicator(
-                          color: Colors.black.withOpacity(0.2),
-                          strokeWidth: 5,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                context.read<GameProvider>().unlimitedMode
+                    ? 'Brickdle Unlimited'
+                    : 'Daily Brickdle #${context.read<GameProvider>().todaysNum}',
+                style: styles.titleTextStyle,
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
+              Image.network(
+                currentLegoSet.imageUrl,
+                fit: BoxFit.fitWidth,
+                height: 300,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress == null) {
+                    return child;
+                  } else {
+                    return Center(
+                      child: SizedBox(
+                        width: 50,
+                        height: 300,
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 125, bottom: 125),
+                          child: CircularProgressIndicator(
+                            color: Colors.black.withOpacity(0.2),
+                            strokeWidth: 5,
+                          ),
                         ),
                       ),
-                    ),
-                  );
-                }
-              },
-            ),
-            Text(
-              '${currentLegoSet.name} ${currentLegoSet.hasSubtheme ? '(${currentLegoSet.subtheme})' : ''}',
-              style: styles.subtitleTextStyle,
-              textAlign: TextAlign.center,
-              softWrap: true,
-            ),
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              IconButton(
-                  onPressed: () {
-                    context.read<GameProvider>().toggleUnlimitedMode();
-                    context.read<GameProvider>().startGame(context);
-                  },
-                  icon: Icon(
-                    Icons.menu,
-                    size: styles.buttonSize,
-                  )),
+                    );
+                  }
+                },
+              ),
+              Text(
+                '${currentLegoSet.name} ${currentLegoSet.hasSubtheme ? '(${currentLegoSet.subtheme})' : ''}',
+                style: styles.subtitleTextStyle,
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                IconButton(
+                    onPressed: () {
+                      context.read<GameProvider>().toggleUnlimitedMode();
+                      context.read<GameProvider>().startGame(context);
+                    },
+                    icon: Icon(
+                      Icons.menu,
+                      size: styles.buttonSize,
+                    )),
+                Padding(
+                  padding: const EdgeInsets.only(right: 16, left: 16),
+                  child: Text(
+                    context.watch<GameProvider>().numOfGuesses.toString(),
+                    style: styles.numberTextStyle,
+                  ),
+                ),
+                context.read<GameProvider>().unlimitedMode
+                    ? IconButton(
+                        onPressed: () {
+                          context.read<GameProvider>().setUnlimitedMode(true);
+                          context.read<GameProvider>().startGame(context);
+                        },
+                        icon:
+                            Icon(Icons.arrow_forward, size: styles.buttonSize),
+                      )
+                    : Opacity(
+                        opacity: context.read<GameProvider>().hasWon ? 1 : 0.15,
+                        child: IconButton(
+                          onPressed: context.read<GameProvider>().hasWon
+                              ? () {
+                                  Clipboard.setData(
+                                      const ClipboardData(text: "I have won"));
+                                }
+                              : null,
+                          icon: Icon(Icons.share, size: styles.buttonSize),
+                        ),
+                      )
+              ]),
               Padding(
-                padding: const EdgeInsets.only(right: 16, left: 16),
-                child: Text(
-                  context.watch<GameProvider>().numOfGuesses.toString(),
-                  style: styles.numberTextStyle,
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: styles.cardWidth,
+                  // height: MediaQuery.of(context).size.width > 768 ? 60 : 40,
+                  height: styles.inputCardHeight,
+                  child: context.watch<GameProvider>().hasWon
+                      ? Text(
+                          '🎉 You won!! 🎉',
+                          style: styles.titleTextStyle,
+                          textAlign: TextAlign.center,
+                          softWrap: true,
+                        )
+                      : TextFormField(
+                          controller: guessTextEditingController,
+                          onFieldSubmitted: (value) {
+                            final valueAsInt = int.tryParse(value);
+                            if (valueAsInt != null) {
+                              context.read<GameProvider>().addGuess(valueAsInt);
+
+                              guessTextEditingController.clear();
+                            }
+                          },
+                          textInputAction: styles.isDesktop
+                              ? TextInputAction.none
+                              : TextInputAction.done,
+                          textAlign: TextAlign.center,
+                          style: styles.numberTextStyle,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15.0),
+                              borderSide: BorderSide.none,
+                            ),
+                            hintText: 'How many bricks?',
+                            hintStyle: styles.subtitleTextStyle,
+                            filled: true,
+                            fillColor: const Color(0x0D000000),
+                          ),
+                        ),
                 ),
               ),
-              context.read<GameProvider>().unlimitedMode
-                  ? IconButton(
-                      onPressed: () {
-                        context.read<GameProvider>().setUnlimitedMode(true);
-                        context.read<GameProvider>().startGame(context);
-                      },
-                      icon: Icon(Icons.arrow_forward, size: styles.buttonSize),
-                    )
-                  : Opacity(
-                      opacity: context.read<GameProvider>().hasWon ? 1 : 0.15,
-                      child: IconButton(
-                        onPressed: context.read<GameProvider>().hasWon
-                            ? () {
-                                Clipboard.setData(
-                                    const ClipboardData(text: "I have won"));
-                              }
-                            : null,
-                        icon: Icon(Icons.share, size: styles.buttonSize),
-                      ),
-                    )
-            ]),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SizedBox(
-                width: styles.cardWidth,
-                // height: MediaQuery.of(context).size.width > 768 ? 60 : 40,
-                height: styles.inputCardHeight,
-                child: context.watch<GameProvider>().hasWon
-                    ? Text(
-                        '🎉 You won!! 🎉',
-                        style: styles.titleTextStyle,
-                        textAlign: TextAlign.center,
-                        softWrap: true,
-                      )
-                    : TextFormField(
-                        controller: guessTextEditingController,
-                        onFieldSubmitted: (value) {
-                          final valueAsInt = int.tryParse(value);
-                          if (valueAsInt != null) {
-                            context.read<GameProvider>().addGuess(valueAsInt);
-
-                            guessTextEditingController.clear();
-                          }
-                        },
-                        textInputAction: styles.isDesktop
-                            ? TextInputAction.none
-                            : TextInputAction.done,
-                        textAlign: TextAlign.center,
-                        style: styles.numberTextStyle,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15.0),
-                            borderSide: BorderSide.none,
-                          ),
-                          hintText: 'How many bricks?',
-                          hintStyle: styles.subtitleTextStyle,
-                          filled: true,
-                          fillColor: const Color(0x0D000000),
-                        ),
-                      ),
-              ),
-            ),
-            ...buildGuessCards(guesses),
-            const SizedBox(
-              height: 100,
-            )
-          ],
+              ...buildGuessCards(guesses),
+              const SizedBox(
+                height: 100,
+              )
+            ],
+          ),
         ),
       ),
-    ));
+    );
   }
 }
